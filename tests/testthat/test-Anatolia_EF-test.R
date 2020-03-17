ecf28fad26cf3d355e48e200899cdff81358f355
Yassine Souilmi <yassinesouilmi@Yassines-iMac-Pro.local>
2020-03-17 16:30:22 +1030

    Udate the anatolia ef test to match testthis package syntax.

--- tests/testthat/test-Anatolia_EF-test.R
+++ tests/testthat/test-Anatolia_EF-test.R
@@ -13,14 +13,17 @@ test_that("Enrichment test and ploting works", {
   gene_scores = merge(gene_scores, output, by = "setName")
   
   library(ggplot2)
-  ggplot(gene_scores, aes(x = setName, y = objStat)) +
-    geom_violin(trim = FALSE, aes(color = setName)) +
-    geom_boxplot(width = 0.15, position = position_dodge(0.9), aes(color = setName)) +
-    geom_dotplot(binaxis = 'y', stackdir = 'center', dotsize = .5, alpha = .35, aes(color = setName, fill = setName)) +
-    scale_x_discrete(breaks=unique(gene_scores[,setName]), 
-                     labels=paste0(unique(gene_scores[,setName]), "\n[p-value = ", unique(gene_scores[,setP]), "]")) +
-    coord_flip() +
-    labs(y="Gene-scores distribution", x = "Gene set/Pathwat", color = "Gene set/Pathway", fill = "Gene set/Pathway") +
-    theme_bw()
+  pp <- ggplot(gene_scores, aes(x = setName, y = objStat)) +
+        geom_violin(trim = FALSE, aes(color = setName)) +
+        geom_boxplot(width = 0.15, position = position_dodge(0.9), aes(color = setName)) +
+        geom_dotplot(binaxis = 'y', stackdir = 'center', dotsize = .5, alpha = .35, aes(color = setName, fill = setName)) +
+        scale_x_discrete(breaks=unique(gene_scores[,setName]), 
+                     labels=paste0(unique(gene_scores[,setName]), "\n[p-value = ", format(unique(gene_scores[,setP]), 5), "]",
+                                   "\n[q-value = ", format(unique(gene_scores[,setQ]), 5), "]")) +
+        coord_flip() +
+        labs(y="Gene-scores distribution", x = "Gene set/Pathwat", color = "Gene set/Pathway", fill = "Gene set/Pathway") +
+        theme_bw()
+  
+  expect_that( pp, is_a("list") )
   
 })
