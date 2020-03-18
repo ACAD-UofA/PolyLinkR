7e693adcaa383597eccba55adb807770d9b6c88a
Yassine Souilmi <yassinesouilmi@Yassines-iMac-Pro.local>
2020-03-18 17:30:33 +1030

    Revising the testthis example

--- tests/testthat/test-Anatolia_EF-test.R
+++ tests/testthat/test-Anatolia_EF-test.R
@@ -1,11 +1,9 @@
 test_that("Enrichment test and ploting works", {
-  output = polylinkr(obj.info = Anatolia_EF_CLR, 
+  expect_that(polylinkr(obj.info = Anatolia_EF_CLR, 
                      set.info = PolyLinkR_SetInfo,
                      set.obj = PolyLinkR_SetObj, 
                      n.cores = 4, 
                      emp.nruns = 10000, 
-                     NN = 1000)
-  
-  expect_that(output, is_a("data.frame") )
-  
+                     NN = 1000), 
+               is_a("data.frame"))
 })
